1315ed4e9b6e1d13e638fa38f5f5673e35d9d3bf
Dayana <dayana.shakenova20@gmail.com>
2023-04-24 20:53:43 +0600

    fix: drawKeyboard function

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,10 @@ module.exports = {
   extends: [
     'eslint-config-airbnb-base',
   ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'linebreak-style': 0,
+  },
 };
--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const symbolsLowRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-
 const symbolsUpperRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Delete', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
 
 document.onkeydown = function (event) {
-  symbolsUpperRu.push(event.key);
-  console.log(symbolsUpperRu);
-}
+  // event.key
+  document.querySelectorAll('[data]').forEach((element) => {
+    element.classList.remove('active');
+  });
+  const char = document.querySelector(`[data="${event.key}"]`);
+  char.classList.add('active');
+};
 
 const drawKeyboard = (arr) => {
   const keyboard = document.createElement('div');
   keyboard.className = 'keyboard';
-  for (let i = 0; i < arr.length; i++) {
-    keyboard.innerHTML += `<div class="char">${arr[i]}</div>`;
+  const keyboardLayout = arr.reduce((acc, curr, index) => {
+    if (['Tab', 'CapsLock', 'Shift', 'Control'].includes(curr) && index !== arr.length - 1 && index !== 54) {
+      acc.push([curr]);
+    } else {
+      acc[acc.length - 1].push(curr);
+    }
+    return acc;
+  }, [[]]);
+  for (let i = 0; i < keyboardLayout.length; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    for (let j = 0; j < keyboardLayout[i].length; j++) {
+      row.innerHTML += `<div class="char" data="${keyboardLayout[i][j]}">${keyboardLayout[i][j]}</div>`;
+    }
+    keyboard.appendChild(row);
   }
-  document.body.prepend(keyboard)
-}
+  document.body.prepend(keyboard);
+};
 
-drawKeyboard(symbolsLowEn)
+drawKeyboard(symbolsLowEn);
